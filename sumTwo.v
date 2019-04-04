edf1cbd907a2e3f345ac24b85e98f2598263b197
Brian Guo <brianguosh@gmail.com>
2019-04-04 11:54:01 -0400

    [add] qed at end of proof

--- sumTwo.v
+++ sumTwo.v
@@ -17,49 +17,42 @@ Section addtwo.
   Definition data := @semantics.data get_contract_type parameter_ty.
   Definition stack := @semantics.stack get_contract_type parameter_ty.
   Definition eval {A B : stack_type} := @semantics.eval _ _ env A B.
-  Definition eval_precond := @semantics.eval_precond _ _ env.
-  Definition ADD_nat {S} : instruction (nat ::: nat ::: S) (nat ::: S) := ADD.
-  About full_contract.
-  About SEQ.
-  Print Implicit full_contract.
+  Definition eval_precond := @semantics.eval_precond _ _ env.  
   Arguments full_contract : default implicits.
   Definition addtwo_contract : full_contract get_contract_type parameter_ty storage_ty := (UNPAIR;; SWAP;; DROP;; UNPAIR;; ADD;; NIL operation;; PAIR).
-  Check precond.
-  Check addtwo_contract.
-  Check Datatypes.nat.
-  Check eval_precond.
-  Check parameter_ty.
-  Check eval.
-  Definition addTwo_spec
-             (param : data parameter_ty)
-             (storage : data storage_ty)
-             (contract: full_contract get_contract_type parameter_ty storage_ty)
-             (fuel : Datatypes.nat)
-             (psi : stack (pair (list_ operation) storage_ty ::: nil) -> Prop)
-    :=
-    (precond (eval contract fuel ((param, storage), tt)), psi).
-  Check data.
-  About N.
-  Check comparable_data storage_ty.
-  Check nat.
-  Check Datatypes.nat.
-  About precond. About eval. 
+  Ltac mysimpl :=
+  match goal with
+    |- ?g =>
+    match g with
+    | context c[semantics.eval_precond ?env (S ?n) ?i ?psi] =>
+      is_var i ||
+             (let simplified := (eval simpl in (semantics.eval_precond env (S n) i psi)) in
+              let full := context c[simplified] in
+              let final := eval cbv beta zeta iota in full in
+              change final)
+    end
+  end.
+
+  Ltac more_fuel :=
+  match goal with
+    | Hfuel : (_ <= ?fuel) |- _ =>
+      destruct fuel as [|fuel];
+      [inversion Hfuel; fail
+      | apply le_S_n in Hfuel]
+  end.
+
   Lemma addTwo_correct (t1 : N) (t2 : N) (storage : N) (fuel : Datatypes.nat) :
     let params : data parameter_ty := (t1, t2) in
     let storage : data storage_ty := storage in
     let sum : N := (t1 + t2)%N in
-    10 <= fuel -> precond (eval (addtwo_contract) (fuel) ((params, storage), tt)) (fun (result : stack (pair (list_ operation) storage_ty ::: nil)) => sum = snd (fst result)) = true.
+    10 <= fuel -> precond (eval (addtwo_contract) (fuel) ((params, storage), tt)) (fun (result : stack (pair (list_ operation) storage_ty ::: nil)) => sum = snd (fst result)).
   Proof.
     intros.
     unfold eval.
-    unfold addtwo_contract.  
-    destruct fuel.
-    - contradict H.
-      easy.
-    - simpl.
-      unfold semantics.eval.
-      destruct 
-      
-      discriminate.
-      trivial.
-    
+    unfold addtwo_contract.   
+    rewrite eval_precond_correct.
+    do 10 more_fuel.
+    mysimpl.
+    simpl.
+    trivial.
+Qed.    
